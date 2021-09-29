9df77dde762ef1cf2c46e6144b8d59d96d0419b3
Dark0o <darko_crnic@hotmail.com>
2021-09-29 22:50:28 +0200

    Add User service

--- ToDo/src/app/login/login.component.ts
+++ ToDo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  username;
+  password;
   
 
+  constructor(private userService: UsersService) { }
+
+  ngOnInit(): void {
+this.userService.getSignedUpUsers().subscribe();
+  }
+
+  onLogIn(username, password){
+
+  }
+
+  onSignUp(username, password){
+    this.userService.addUser({username: username, password: password}).subscribe(data =>{
+      console.log(data);
+    })
+  }
 }
---
+++ ToDo/src/app/services/users.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  users = [];
+
+  constructor(private http: HttpClient) { }
+
+  getSignedUpUsers() : Observable<any>{
+  
+  return this.http.get('https://todo-app-2e14b-default-rtdb.europe-west1.firebasedatabase.app/users.json')
+                  .pipe(
+                      map(data => {
+                        console.log(data);
+                        for(const key in data){
+                          this.users.push({...data[key], id: key});
+                         }
+                         console.log(this.users);
+                         
+                        return this.users;
+                      }
+                      ),
+                  );
+  }
+
+  addUser(user) : Observable<any>{
+
+    return this.http.post('https://todo-app-2e14b-default-rtdb.europe-west1.firebasedatabase.app/users.json', user);
+  }
+
+
+  userExists(username: string, password: string) {
+    let existingUser = this.users.filter(
+      (u) => u.username === username && u.password === password
+    );
+    if (existingUser.length === 1) {
+      return true;
+    }
+    return false;
+  }
+
+
+}
