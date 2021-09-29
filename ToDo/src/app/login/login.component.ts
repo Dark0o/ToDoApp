8e007e1f1099872ed95c78c3615259043255258b
Dark0o <darko_crnic@hotmail.com>
2021-09-29 23:07:24 +0200

    Add Auth service and User log in

--- ToDo/src/app/login/login.component.ts
+++ ToDo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 import { UsersService } from '../services/users.service';
 
 @Component({
@@ -13,17 +14,21 @@ export class LoginComponent implements OnInit {
   password;
   
 
-  constructor(private userService: UsersService) { }
+  constructor(private userService: UsersService, private authService: AuthService) { }
 
   ngOnInit(): void {
 this.userService.getSignedUpUsers().subscribe();
   }
 
   onLogIn(username, password){
-
+this.authService.login(username, password);
   }
 
   onSignUp(username, password){
+    if(username === '' || password === ''){
+      alert('Please enter e-mail and password');
+      return;
+    }
     this.userService.addUser({username: username, password: password}).subscribe(data =>{
       console.log(data);
     })
