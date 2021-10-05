6bcfba8d555b4ef3a64a025124dfe7919015c146
Dark0o <darko_crnic@hotmail.com>
2021-10-06 00:00:33 +0200

    Get the users full name from the users list and add it to todo object

--- ToDo/src/app/components/shared-todos-list/shared-todos-list.component.ts
+++ ToDo/src/app/components/shared-todos-list/shared-todos-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToDoService } from 'src/app/services/todo.service';
+import { UsersService } from 'src/app/services/users.service';
+import { DateFormatter } from 'src/app/DateFormatter';
 
 @Component({
   selector: 'app-shared-todos-list',
@@ -10,11 +12,25 @@ export class SharedTodosListComponent implements OnInit {
   todo;
   sharedTodos = [];
 
-  constructor(private todosService: ToDoService) {}
+  constructor(
+    private todosService: ToDoService,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit(): void {
     this.todosService.getToDos().subscribe((todos) => {
       this.sharedTodos = todos.filter((todo) => todo.isPublic === true);
+      this.sharedTodos.map((todo) => {
+        let foundUser = this.usersService.users.find(
+          (user) => user.id === todo.userID
+        );
+        if (foundUser.fullName) {
+          todo.fullName = foundUser.fullName;
+        }
+        todo.createdAt = DateFormatter.formatDate(todo.createdAt);
+        console.log(todo);
+        return todo;
+      });
       console.log(this.sharedTodos);
     });
   }
