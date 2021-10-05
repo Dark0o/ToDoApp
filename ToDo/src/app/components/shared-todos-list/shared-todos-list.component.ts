73e6fa40c7f682581a50659344572a835b6e3537
Dark0o <darko_crnic@hotmail.com>
2021-10-05 22:12:57 +0200

    Get and display shared todos

--- ToDo/src/app/components/shared-todos-list/shared-todos-list.component.ts
+++ ToDo/src/app/components/shared-todos-list/shared-todos-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-shared-todos-list',
   templateUrl: './shared-todos-list.component.html',
-  styleUrls: ['./shared-todos-list.component.scss']
+  styleUrls: ['./shared-todos-list.component.scss'],
 })
 export class SharedTodosListComponent implements OnInit {
+  todo;
+  sharedTodos = [];
 
-  constructor() { }
+  constructor(private todosService: ToDoService) {}
 
   ngOnInit(): void {
+    this.todosService.getToDos().subscribe((todos) => {
+      this.sharedTodos = todos.filter((todo) => todo.isPublic === true);
+      console.log(this.sharedTodos);
+    });
   }
 
+  onAddToDo(todo) {}
 }
